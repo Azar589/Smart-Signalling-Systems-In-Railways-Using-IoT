459865c59ef9400ad1a9a13d126050c68db99115
Azar589 <mohammedazar4458730@gmail.com>
2026-03-11 15:08:12 +0530

    Code Updated for stablity

--- Project/Rx_Unit.cpp
+++ Project/Rx_Unit.cpp
@@ -2,60 +2,59 @@
 #include <SPI.h>
 #include <WiFi.h>
 #include <HTTPClient.h>
-RH_ASK rf_driver(2000, 16, -1, -1);  // (bitrate, RX, TX, PTT)
+
+RH_ASK rf_driver(2000, 16, -1, -1);
 WiFiClient client;
 HTTPClient http;
-int num;
-//LiquidCrystal lcd(12, 11, 5, 4, 3, 2);
+
+unsigned long lastUploadTime = 0;
+const long uploadInterval = 16000; // 16 seconds for ThingSpeak free tier
+
 void ConnectToWIFI() {
   WiFi.mode(WIFI_STA);
   WiFi.begin("Azar's M34", "azar@2005");
-  Serial.print("Connecting to WiFi");
   while (WiFi.status() != WL_CONNECTED) {
+    delay(500);
     Serial.print(".");
-    delay(200);
   }
-  Serial.println();
-  Serial.println("IP Address");
-  Serial.println(WiFi.localIP());
-  Serial.println("MAC Address");
-  Serial.println(WiFi.macAddress());
+  Serial.println("\nWiFi Connected");
 }
+
 void thingSpeakWrite(String data) {
-  String url = "http://api.thingspeak.com/update?api_key=12WTGAIJQYNRUL22&field1=" + data;
-  http.begin(client, url);
-  Serial.println("Waiting for Response...");
-  int httpcode = http.GET();
- 
-  if (httpcode > 0) {
-    Serial.println("Data Sent Successfully: " + data);
+  // Check if enough time has passed since last upload
+  if (millis() - lastUploadTime > uploadInterval) {
+    String url = "http://api.thingspeak.com/update?api_key=12WTGAIJQYNRUL22&field1=" + data;
+    http.begin(client, url);
+    int httpcode = http.GET();
+    
+    if (httpcode > 0) {
+      Serial.println("Cloud Sync Successful: " + data);
+      lastUploadTime = millis();
+    } else {
+      Serial.println("HTTP Error: " + String(httpcode));
+    }
+    http.end();
   } else {
-    Serial.println("Error Sending Data");
+    Serial.println("Upload throttled (Waiting for 15s limit)");
   }
- 
-  http.end();
 }
+
 void setup() {
   Serial.begin(115200);
-  rf_driver.init();
+  if (!rf_driver.init()) Serial.println("RF Init Failed");
   ConnectToWIFI();
-  Serial.print("\nSIGNAL STATUS");
-  Serial.print("\n");
-  //lcd.begin(16, 2);
-  //lcd.print("SIGNAL STATUS");
 }
+
 void loop() {
-  String dataToSend;
-  uint8_t buf[RH_ASK_MAX_MESSAGE_LEN];
-  delay(100);
-  uint8_t buflen = sizeof(buf);
-  if (rf_driver.recv(buf, &buflen))
-  {
-    buf[buflen] = '\0';
-    Serial.print("\nReceived: ");
-    Serial.println((char*)buf);
-    dataToSend = (char*)buf;
-    delay(500);
-    thingSpeakWrite(dataToSend);
+  // Max message length + 1 for null terminator to prevent overflow
+  uint8_t buf[RH_ASK_MAX_MESSAGE_LEN + 1]; 
+  uint8_t buflen = sizeof(buf) - 1;
+
+  if (rf_driver.recv(buf, &buflen)) {
+    buf[buflen] = '\0'; // Properly terminate the string
+    String receivedData = (char*)buf;
+    Serial.println("Received RF: " + receivedData);
+    
+    thingSpeakWrite(receivedData);
   }
 }
--- Project/Tx_Unit.cpp
+++ Project/Tx_Unit.cpp
@@ -1,49 +1,47 @@
 #include <RH_ASK.h>
 #include <SPI.h>
-RH_ASK rf_driver(2000, -1, 4, -1);  // (bitrate, RX, TX, PTT)
-const int red_l=0;
-const int yellow_l=2;
-const int green_l=14;
-const int red_s=5;
-const int yellow_s=12;
-const int green_s=13;
+
+RH_ASK rf_driver(2000, -1, 4, -1); 
+
+const int red_l=0, yellow_l=2, green_l=14;
+const int red_s=5, yellow_s=12, green_s=13;
+
+String lastStatus = ""; // To track the previous state
+
 void setup() {
   Serial.begin(115200);
-  rf_driver.init();
-  pinMode(red_l,OUTPUT);
-  pinMode(yellow_l,OUTPUT);
-  pinMode(green_l,OUTPUT);
-  pinMode(red_s,INPUT);
-  pinMode(yellow_s,INPUT);
-  pinMode(green_s,INPUT);
+  if (!rf_driver.init()) Serial.println("RF Init Failed");
+  
+  pinMode(red_l, OUTPUT); pinMode(yellow_l, OUTPUT); pinMode(green_l, OUTPUT);
+  pinMode(red_s, INPUT); pinMode(yellow_s, INPUT); pinMode(green_s, INPUT);
 }
+
 void loop() {
-  if(digitalRead(red_s) == HIGH)
-  {
-    digitalWrite(red_l,HIGH);
-    const char *msg = "RED";
+  String currentStatus = "";
+
+  // Determine current signal state
+  if(digitalRead(red_s) == HIGH) {
+    digitalWrite(red_l, HIGH);
+    currentStatus = "RED";
+  } else digitalWrite(red_l, LOW);
+
+  if(digitalRead(yellow_s) == HIGH) {
+    digitalWrite(yellow_l, HIGH);
+    currentStatus = "YELLOW";
+  } else digitalWrite(yellow_l, LOW);
+
+  if(digitalRead(green_s) == HIGH) {
+    digitalWrite(green_l, HIGH);
+    currentStatus = "GREEN";
+  } else digitalWrite(green_l, LOW);
+
+  // Only send if the status has changed and isn't empty
+  if (currentStatus != "" && currentStatus != lastStatus) {
+    const char *msg = currentStatus.c_str();
     rf_driver.send((uint8_t *)msg, strlen(msg));
-    Serial.println("Message Sent: RED");
-  }else{
-    digitalWrite(red_l,LOW);
+    rf_driver.waitPacketSent();
+    Serial.println("Change Detected. Message Sent: " + currentStatus);
+    lastStatus = currentStatus;
   }
-  if(digitalRead(yellow_s) == HIGH)
-  {
-    digitalWrite(yellow_l,HIGH);
-    const char *msg = "YELLOW";
-    rf_driver.send((uint8_t *)msg, strlen(msg));
-    Serial.println("Message Sent: YELLOW");
-  }else{
-    digitalWrite(yellow_l,LOW);
-  }
-  if(digitalRead(green_s) == HIGH)
-  {
-    digitalWrite(green_l,HIGH);
-    const char *msg = "GREEN";
-    rf_driver.send((uint8_t *)msg, strlen(msg));
-    Serial.println("Message Sent: GREEN");
-  }else{
-    digitalWrite(green_l,LOW);
-  }
-  rf_driver.waitPacketSent();
+  delay(50); // Small debounce/stability delay
 }
